84693d934598476d4b7f590b424c2716defe8175
Oreology <hafidhsatyanto@gmail.com>
2019-07-17 15:20:02 -0500

    initialize catkin workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/hafidh/Development/ROSdev/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/hafidh/ws_moveit/devel;/opt/ros/melodic".split(';') if "/home/hafidh/ws_moveit/devel;/opt/ros/melodic" != "" else []
